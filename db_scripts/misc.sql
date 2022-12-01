f7a15b2c190aea9f748554d0a53fa1d92eb32421
TejasBhitle <bhitletejas@gmail.com>
2022-12-01 01:12:16 -0500

    schema added

--- db_scripts/misc.sql
+++
@@ -1,9 +0,0 @@
-
--- find out constraints on a table in SQL
-SELECT con.*
-       FROM pg_catalog.pg_constraint con
-            INNER JOIN pg_catalog.pg_class rel
-                       ON rel.oid = con.conrelid
-            INNER JOIN pg_catalog.pg_namespace nsp
-                       ON nsp.oid = connamespace
-       WHERE rel.relname = '<table name>';
